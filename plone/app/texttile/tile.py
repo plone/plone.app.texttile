5fc9751eb5248d98c6180702f8f38be3b0fed9fd
Rok Garbas <rok@garbas.si>
2012-03-26 12:30:54 +0200

    added schema for texttile

--- plone/app/texttile/tile.py
+++ plone/app/texttile/tile.py
@@ -1,5 +1,16 @@
+from zope.schema import Text
+from zope.interface import Interface
+from zope.i18nmessageid import MessageFactory
 from plone.tiles.tile import PersistentTile
+
+
+_ = MessageFactory('plone')
+
+
+class ITextTile(Interface):
+    text = Text(title=_("Tile text"))
 
 
 class TextTile(PersistentTile):
     """ A text tile """
+
