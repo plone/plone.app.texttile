678a994e697f72bf39774ca7ae4883557440df50
Rok Garbas <rok@garbas.si>
2012-04-17 09:47:30 +0200

    adding cleditor since tinymce was impossible to set to work with iframe (until i get tinymce working cleditor should be sufficient)

--- plone/app/texttile/tile.py
+++ plone/app/texttile/tile.py
@@ -1,13 +1,15 @@
 from zope.schema import Text
-from zope.interface import Interface
 from zope.i18nmessageid import MessageFactory
+from plone.directives import form
+from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 from plone.tiles.tile import PersistentTile
-
 
 _ = MessageFactory('plone')
 
 
-class ITextTile(Interface):
+class ITextTile(form.Schema):
+
+    form.widget(text=WysiwygFieldWidget)
     text = Text(title=_("Tile text"))
 
 
