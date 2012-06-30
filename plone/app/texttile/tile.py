3873fac9ab1bdce8c3899803cf450cb3dfdfc8ad
Rok Garbas <rok@garbas.si>
2012-06-30 08:05:00 +0200

    extend texttile from main tile schema

--- plone/app/texttile/tile.py
+++ plone/app/texttile/tile.py
@@ -1,16 +1,18 @@
 from zope.schema import Text
 from zope.i18nmessageid import MessageFactory
-from plone.directives import form
-from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
+from plone.autoform import directives as form
 from plone.tiles.tile import PersistentTile
+from plone.app.tiles.interfaces import ITileBaseSchema
 
 _ = MessageFactory('plone')
 
 
-class ITextTile(form.Schema):
+class ITextTile(ITileBaseSchema):
 
-    form.widget(text=WysiwygFieldWidget)
-    text = Text(title=_("Tile text"))
+    form.widget(text='plone.app.z3cform.wysiwyg.WysiwygFieldWidget')
+    text = Text(
+        title=_("Tile text"),
+        )
 
 
 class TextTile(PersistentTile):
